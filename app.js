bae8e427a8c51dab8745d43be689be4e3c3d396a
mehdishokrani <mehdishokrani1@gmail.com>
2023-07-02 07:42:21 -0600

    Add EJS and also initiate its commands

--- app.js
+++ app.js
@@ -1,19 +1,44 @@
 const express  =require("express")
 const bodyParser = require("body-parser")
+
 const port = 3000
 const app = express()
 
+app.set('view engine','ejs')
+
 var today = new Date();
 var currentDay = today.getDay();
-
+var dayOfWeek = ""
 
 
 app.get("/",(req,res)=>{
-    if(today === 6 || today === 0)
-    res.send(`Holiday<br> it is ${today.getMonth()}`)
-    else{
-        res.sendFile(__dirname+"/index.html")
+    switch(currentDay){
+        case 0:
+            dayOfWeek = "Sunday"
+            break
+        case 1:
+            dayOfWeek = "Monday"
+            break
+        case 2:
+            dayOfWeek = "Tusday"
+            break
+        case 3:
+            dayOfWeek = "Wednesday"
+            break
+        case 4:
+            dayOfWeek = "Thursday"
+            break
+        case 5:
+            dayOfWeek = "Friday"
+            break
+        case 6:
+            dayOfWeek = "Saturday"
+            break
+        default:
+            dayOfWeek = "Wrong Return Value"
+            break
     }
+    res.render("list",{Today: dayOfWeek})
 })
 
 
