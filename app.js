c759622d58c13e88a533fa85a61d08cfee6f8b1c
mehdishokrani <mehdishokrani1@gmail.com>
2023-07-03 11:07:37 -0600

    change all Var to Let in app.js

--- app.js
+++ app.js
@@ -7,22 +7,22 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
 
-var items = ["Buy Food","Cook Food","Eat Food"];
+let items = ["Buy Food","Cook Food","Eat Food"];
 
-var today = new Date();
-var option = {
+let today = new Date();
+let option = {
   weekday: "long",
   month: "long",
   day: "numeric",
 };
 
 app.post("/", (req, res) => {
-  var item = req.body.newItem;
+  let item = req.body.newItem;
   items.push(item);
   res.redirect("/");
 });
 
-var dayOfWeek = today.toLocaleDateString("en-US", option);
+let dayOfWeek = today.toLocaleDateString("en-US", option);
 
 app.get("/", (req, res) => {
   res.render("list", { DayOfWeek: dayOfWeek, newItem: items });
