15d4243e0c8360f22438667ac80ad7ad6e73fb82
mehdishokrani <mehdishokrani1@gmail.com>
2023-07-03 12:03:25 -0600

    add work list to list.js

--- app.js
+++ app.js
@@ -5,11 +5,12 @@ const port = 3000;
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("public"))
+app.use(express.static("public"));
 
 app.set("view engine", "ejs");
 
-let items = ["Buy Food","Cook Food","Eat Food"];
+let items = ["Buy Food", "Cook Food", "Eat Food"];
+let workItems = [];
 
 let today = new Date();
 let option = {
@@ -20,14 +21,23 @@ let option = {
 
 app.post("/", (req, res) => {
   let item = req.body.newItem;
-  items.push(item);
-  res.redirect("/");
+  if (req.body.btn === "Work List") {
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
 });
 
 let dayOfWeek = today.toLocaleDateString("en-US", option);
 
 app.get("/", (req, res) => {
-  res.render("list", { DayOfWeek: dayOfWeek, newItem: items });
+  res.render("list", { listTitle: dayOfWeek, newItem: items });
+});
+
+app.get("/work", (req, res) => {
+  res.render("list", { listTitle: "Work List", newItem: workItems });
 });
 
 app.listen(port, () => {
