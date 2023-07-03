156575961f4963db6b0c4f98d097268c616269df
mehdishokrani <mehdishokrani1@gmail.com>
2023-07-03 11:42:29 -0600

    add stayle.css in public folder and call it from app.js

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const bodyParser = require("body-parser");
 
 const port = 3000;
 const app = express();
+
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"))
 
 app.set("view engine", "ejs");
 
