521dff46cc4af50582addb0dd7a326369f9173bb
mehdishokrani <mehdishokrani1@gmail.com>
2023-07-21 04:08:07 -0600

    add dynamic todolist in url with separate mongodb database

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const mongoose = require("mongoose")
-const date = require(__dirname+"/date.js")
+const mongoose = require("mongoose");
+const date = require(__dirname + "/date.js");
 
 const port = 3000;
 const app = express();
@@ -9,73 +9,112 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://127.0.0.1:27017/todolistDB")
+mongoose.connect("mongodb://127.0.0.1:27017/todolistDB");
 
 app.set("view engine", "ejs");
 
 const itemsSchema = new mongoose.Schema({
-  name: String
-})
+  name: String,
+});
 
-const Item = mongoose.model("item",itemsSchema) //Creat collection
+const listSchema = new mongoose.Schema({
+  name: String,
+  items: [itemsSchema],
+});
+
+const Item = mongoose.model("item", itemsSchema); //Creat collection
+const List = mongoose.model("list", listSchema);
 
 const item1 = new Item({
-  name:"Welcome to your ToDo list"
-})
+  name: "Welcome to your ToDo list",
+});
 const item2 = new Item({
-  name:"Hit + button to add new item"
-})
+  name: "Hit + button to add new item",
+});
 const item3 = new Item({
-  name:"<-- Hit this to delete an item"
-})
-
-const defaultItems = [item1,item2,item3]
-
-Item.deleteMany({})
-  .then(() => {
-    Item.insertMany(defaultItems).then(result=>{
-      console.log("Document inserted: ",result)
-    }).catch(err =>{
-      console.error("Error inserting documents: ", err);
-    })
-  })
-  .catch(error => {
-    console.error('Error clearing collection:', error);
-  });
-
-
-
-
-// const items = ["Buy Food", "Cook Food", "Eat Food"];
-// const workItems = [];
-
-app.post("/", (req, res) => {
-  let item = req.body.newItem;
-  if(item != ""){
-  if (req.body.btn === "Work List") {
-    workItems.push(item);
-    res.redirect("/work");
-  } else {
-    items.push(item);
-    res.redirect("/");
-  }
-}
+  name: "<-- Hit this to delete an item",
 });
 
+const defaultItems = [item1, item2, item3];
 
+const dayOfWeek = date.getDay();
 
 app.get("/", (req, res) => {
-    const dayOfWeek = date.getDay()
-  res.render("list", { listTitle: dayOfWeek, newItem: items });
+  
+  Item.find({}).then((result) => {
+    items = result;
+    if (items.length === 0) {
+      Item.deleteMany({})
+        .then(() => {
+          Item.insertMany(defaultItems)
+            .then((result) => {
+              console.log("Document inserted Successfully");
+            })
+            .catch((err) => {
+              console.error("Error inserting documents: ", err);
+            });
+        })
+        .catch((error) => {
+          console.error("Error clearing collection:", error);
+        });
+      res.redirect("/");
+    } else {
+      res.render("list", { listTitle: dayOfWeek, newItem: items });
+    }
+  });
 });
 
-app.get("/work", (req, res) => {
-  res.render("list", { listTitle: "Work List", newItem: workItems });
+app.get("/:customeListName", (req, res) => {
+  const customeListName = req.params.customeListName;
+
+  List.findOne({ name: customeListName.toLowerCase() }).then((result) => {
+    if(!result){
+      const list = new List({
+        name: customeListName.toLowerCase(),
+        items: defaultItems
+      })
+      list.save()
+      res.redirect("/"+customeListName)
+    }else{
+      res.render("list", {listTitle: customeListName.toUpperCase(), newItem: result.items })
+    }
+  });
+    
+
+  
 });
 
-app.get("/about",(req,res)=>{
-    res.render("about")
-})
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+
+app.post("/", (req, res) => {
+  let newItem = req.body.newItem;
+  const listName = (req.body.btn).toLowerCase()
+  if (newItem != "") {
+    const item = new Item({
+      name: newItem,
+    });
+
+    if(listName === dayOfWeek){
+      item.save()
+      res.redirect("/")
+    }else{
+      List.findOne({name: listName}).then((result)=>{
+        result.items.push(item)
+        result.save()
+        res.redirect("/"+listName)
+      })
+    }
+    
+  }
+});
+
+app.post("/delete", (req, res) => {
+  const checkedItamID = req.body.checkBox;
+  Item.findByIdAndRemove(checkedItamID).exec();
+  res.redirect("/");
+});
 
 app.listen(port, () => {
   console.log(`Server started at port ${port}`);
